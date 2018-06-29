8ef4b8924e8c54d89fb5207e91917784f341b1fd
y95847frank <b03902052@ntu.edu.tw>
2018-06-29 22:30:13 +0800

    ensemble_f1:80.85%

--- nick_src/main.py
+++ nick_src/main.py
@@ -33,7 +33,7 @@ label_map = ['Other',
                 'Content-Container(e1,e2)' , 'Content-Container(e2,e1)'
                 ]
 embedding = np.load('emb.npy')
-print(embedding.shape)
+print('embeding',embedding.shape)
 
 modOpts = {
         'maxsen_len' : max_sentence_length,
@@ -77,41 +77,41 @@ with open('word2IDx.pickle', 'rb') as f:
     word2IDx = pickle.load(f)
 
     train_token, train_pos1, train_pos2, train_lexical, train_y = create_data(word2IDx, 'train', pos_pos1_list, pos_pos2_list)
-    count = 0
-    for i in range(train_y.shape[0]):
-        if train_y[i] == 0:
-            count += 1
-    print('count:', count)
-    #class_weight_dict = {}
-    #print(class_weight_dict)
     class_weights = class_weight.compute_class_weight('balanced', np.unique(train_y), train_y)
-    print(class_weights)
+    #print(class_weights)
     class_weight_dict = dict(enumerate(class_weights))
-    print(class_weight_dict)
+    #print(class_weight_dict)
 
-    train_y = np_utils.to_categorical(train_y, n_out)
+    train_y = np_utils.to_categorical(train_y, n_out)    
 
     test_token, test_pos1, test_pos2, test_lexical = create_data(word2IDx, 'test', pos_pos1_list, pos_pos2_list)
     (train_token, val_token), (train_pos1, val_pos1), (train_pos2, val_pos2), (train_lexical, val_lexical), (train_y, val_y) = split_data(train_token, train_pos1, train_pos2, train_y, train_lexical,  0.2)
-    
+     
     if args.train:
-        filepath = "weights_CNN_best_unstable_wordemb_POS1_POS2.h5"
+        filepath = "weights_CNN_best_unstable_wordemb_POS1_POS2_final.h5"
         checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose = 1, save_best_only=True, mode='min')
         earlystop = EarlyStopping(monitor='val_loss', patience=2, mode='min')
         callbacks_list = [checkpoint, earlystop]
-        model.fit([train_pos1, train_pos2, train_token, train_lexical], train_y, epochs=num_epoch, batch_size=batch, validation_data = ([val_pos1, val_pos2, val_token, val_lexical], val_y), class_weight=class_weight_dict,callbacks=callbacks_list, verbose=1)
+        model.fit([train_pos1, train_pos2, train_token, train_lexical], train_y, epochs=num_epoch, batch_size=batch, validation_data = ([val_pos1, val_pos2, val_token, val_lexical], val_y), class_weight=class_weight_dict, callbacks=callbacks_list, verbose=1)
     else:
-        #model.load_weights('best.h5')
-        model.load_weights('weights_CNN_best_unstable_wordemb_POS1_POS2.h5')
+        #model.load_weights('best1.h5')
+        '''
+        model.load_weights('weights_CNN_best_unstable_wordemb_POS1_POS2_final.h5')
         predict = model.predict([test_pos1, test_pos2, test_token, test_lexical])
-        predict_cls = np.argmax(predict, axis=1)
-        print(predict_cls.shape)
-        print(predict_cls[208])
-        print(label_map[17])
+        np.save('model_3.npy', predict)
+        '''
+        model_1 = np.load('model_1.npy')
+        model_2 = np.load('model_2.npy')
+        model_3 = np.load('model_3.npy')
+
+        final_model = np.add(np.add(model_1, model_2), model_3)
+        print(final_model.shape)
+        predict_cls = np.argmax(final_model, axis=1)
+        
         count = 8001
         with open('../dataset/myans.txt','w') as f:
             for item in range(predict_cls.shape[0]): #17018
                 #print(item)
                 f.write(str(count) + '\t' + label_map[predict_cls[item]] + '\n')
                 count += 1
-
+ 
