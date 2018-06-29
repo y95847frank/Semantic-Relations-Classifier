00dd7e88c35b9a4e8afcf38b868325b828a2d5cf
y95847frank <b03902052@ntu.edu.tw>
2018-06-29 15:31:09 +0800

    CNN_lotsoffeatures_f1:81.40%

--- nick_src/CNN.py
+++ nick_src/CNN.py
@@ -1,6 +1,6 @@
 import keras
 
-from keras.layers import Input, Conv1D, Dropout, Merge, Dense, Activation, Dense
+from keras.layers import Input, Conv1D, Dropout, Merge, Dense, Activation, Dense, Flatten, Reshape
 from keras.models import Model
 from keras.layers.pooling import GlobalMaxPooling1D
 from keras.layers.embeddings import Embedding
@@ -25,14 +25,35 @@ class CNN:
         input3 = Input(shape = (self.options['maxsen_len'], ))
         z = Embedding(self.options['vocab_size'], self.options['emb_dim'], input_length = self.options['maxsen_len'], weights = [self.options['embedding']], trainable=False)(input3)
         
+
+        #input4 = Input(shape = (1, ))
+        #a = Embedding(self.options['POS_size'], self.options['emb_dim'], input_length = 1)(input4)
+
+        #input5 = Input(shape = (1, ))
+        #b = Embedding(self.options['POS_size'], self.options['emb_dim'], input_length = 1)(input5)
+        
+        input6 = Input(shape = (8, ))
+        c = Embedding(self.options['vocab_size'], self.options['emb_dim'], input_length = 8, weights = [self.options['embedding']], trainable=False)(input6)
+
         merge = concatenate([x, y, z])
 
-        submodel = Conv1D(self.options['num_filter'], 2, padding='valid', activation='tanh', input_shape=(self.options['maxsen_len'], self.options['emb_dim'] + self.options['pos_dim'] * 2 ))(merge)
+        submodel = Conv1D(self.options['num_filter'], 2, padding='valid', input_shape=(self.options['maxsen_len'], self.options['emb_dim'] + self.options['pos_dim'] * 2 ))(merge)
         
         submodel = GlobalMaxPooling1D()(submodel)
+
+        submodel = Reshape((1, 500))(submodel)
+
+        submodel = Dense(300, activation='tanh')(submodel)
         
-        final_merge = Dropout(0.25)(submodel)
+        final_merge = concatenate([c, submodel], axis = 1)
+
+        final_merge = Flatten()(final_merge)
+        
+        #final_merge = Dropout(0.25)(final_merge)
+        
+        #output = Dense(128, activation='relu')(final_merge)
         output = Dense(self.options['n_class'], activation='softmax')(final_merge)
 
-        return Model(inputs=[input1, input2, input3], outputs = output)
+
+        return Model(inputs=[input1, input2, input3, input6], outputs = output)
         
--- nick_src/preprocess.py
+++ nick_src/preprocess.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import re
 import pickle
+import nltk
+from nltk.corpus import wordnet as wn
 ### label mapping
 label_map = [ 'Other',
         'Message-Topic(e1,e2)', 'Message-Topic(e2,e1)' ,
@@ -27,19 +29,30 @@ def load_train_data(path):
     with open(path, 'r') as f:
         content = f.readlines()
     content = [x for x in content]
-    for idx in range(0, len(content), 4):
+    count = 0
+    for idx in range(0, len(content), 4): #len(content)
         words = content[idx].split('\t')[1][1:-3]
+        words = words.replace('<e1>', '_e1').replace('</e1>','e1')
+        words = words.replace('<e2>', '_e2').replace('</e2>','e2')
+        words = clear_str(words)
         split_word = words.split()
+        pos1_flag = False
+        pos2_flag = False
         for word in split_word:
-            if '<e1>' in word:
+            if 'e1' in word and pos1_flag == False:
                 pos_e1 = split_word.index(word)
                 train_Pos1.append(pos_e1)
-            if '<e2>' in word:
+                pos1_flag = True
+            if 'e2' in word and pos2_flag == False:
                 pos_e2 = split_word.index(word)
                 train_Pos2.append(pos_e2)
-        words = words.replace('<e1>','').replace('</e1>','') 
-        words = words.replace('<e2>','').replace('</e2>','')
-        words = clear_str(words)
+                pos2_flag = True
+        if count == 2134:
+            print(words)
+            print(pos_e1, pos_e2)
+        count += 1
+        words = words.replace('e1','')
+        words = words.replace('e2','')
         train_word.append(words)
         relation = content[idx+1].strip()
         if relation in label_map:
@@ -49,24 +62,29 @@ def load_test_data(path):
     test_word = []
     test_Pos1 = []
     test_Pos2 = []
-    count = 0
     with open(path, 'r') as f:
         for line in f:
             _, sentence = line.split('\t')
             words = sentence[1:-3]
+            words = words.replace('<e1>', '_e1').replace('</e1>','e1')
+            words = words.replace('<e2>', '_e2').replace('</e2>','e2')
+            words = clear_str(words)
             split_word = words.split()
+            pos1_flag = False
+            pos2_flag = False
             for word in split_word:
-                if '<e1>' in word:
+                if 'e1' in word and pos1_flag == False:
                     pos_e1 = split_word.index(word)
                     test_Pos1.append(pos_e1)
-                if '<e2>' in word:
+                    pos1_flag = True
+                if 'e2' in word and pos2_flag == False:
                     pos_e2 = split_word.index(word)
                     test_Pos2.append(pos_e2)
-            words = words.replace('<e1>','').replace('</e1>','') 
-            words = words.replace('<e2>','').replace('</e2>','')
+                    pos2_flag = True
+            words = words.replace('e1','')
+            words = words.replace('e2','')
             words = clear_str(words)
             test_word.append(words)
-            count += 1
     return test_word, test_Pos1, test_Pos2
 
 def clear_str(string):
@@ -89,17 +107,44 @@ def clear_str(string):
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
     
-def create_wordset(train_word, test_word):
+def create_wordset(train_word):
     wordset = set()
     for sentence in train_word:
         for word in sentence.split():
             wordset.add(word)
+            try:
+                wordset.add(wn.synsets(word)[0].hypernyms()[0].name().split('.')[0])
+            except:
+                a = 0
     return list(wordset)
+
+def create_pos_set(train_word, train_pos1, train_pos2):
+    pos1_list = list()
+    pos2_list = list()
+    count = 0
+    for sentence in train_word:
+        pos_position1 = nltk.pos_tag(sentence.split())[train_pos1[count]][1] 
+        pos_position2 = nltk.pos_tag(sentence.split())[train_pos2[count]][1]
+
+        if pos_position1 not in pos1_list:
+            pos1_list.append(pos_position1)
+        if pos_position2 not in pos2_list:
+            pos2_list.append(pos_position2)
+        count += 1
+    print(pos1_list)
+    print(pos2_list)
+
+    with open('pos1_list.pkl','wb') as f:
+        pickle.dump(pos1_list, f)
+    with open('pos2_list.pkl','wb') as f:
+        pickle.dump(pos2_list, f)
+    
+    #return pos1_list, pos2_list
 
 def create_embedding(embedding_path,train_word, test_word):
     word2Idx = {}
     embeddings = []
-    words = create_wordset(train_word, test_word)
+    words = create_wordset(train_word)
     print('len words:',len(words))
     count = 0
     for line in open(embedding_path):
@@ -124,8 +169,15 @@ def create_embedding(embedding_path,train_word, test_word):
     np.save('emb.npy', embeddings)
     with open('word2IDx.pickle', 'wb') as f:
         pickle.dump(word2Idx, f)
+def get_pos_index(pos_list, word):
+    word = list(word)
+    pos = nltk.pos_tag(word)[0][1]
+    if pos not in pos_list:
+        return 0
+    else:
+        return pos_list.index(pos) + 1
 
-def create_data(word2Idx, flag):
+def create_data(word2Idx, flag, pos_pos1_list, pos_pos2_list):
     if flag == 'train':
         words, pos1, pos2, label = load_train_data(train_path)
         label = np.array(label)
@@ -134,12 +186,57 @@ def create_data(word2Idx, flag):
     tokenMatrix = []
     position1Matrix = []
     position2Matrix = []
+    position1_5Word = [] 
+    pos_position1Matrix = []
+    pos_position2Matrix = []
+    
     for _id in range(len(words)):
+        wordvec = []
         tokens = np.zeros(maxsen_len)
         pos1Value = np.zeros(maxsen_len)
         pos2Value = np.zeros(maxsen_len)
         e1 = pos1[_id]
         e2 = pos2[_id]
+
+        
+        got_index = getWordIdx(words[_id].split()[e1], word2Idx)
+        wordvec.append(got_index)
+        got_index = getWordIdx(words[_id].split()[e2], word2Idx)
+        wordvec.append(got_index)
+        
+        tmp_e1 = words[_id].split()[e1]
+        tmp_e2 = words[_id].split()[e2]
+        try:
+            wordvec.append(getWordIdx(wn.synsets(tmp_e1)[0].hypernyms()[0].name().split('.')[0], word2Idx))
+        except:
+            wordvec.append(e1)
+        try:
+            wordvec.append(getWordIdx(wn.synsets(tmp_e2)[0].hypernyms()[0].name().split('.')[0], word2Idx))
+        except:
+            wordvec.append(e2)
+        
+        if e1 == 0:
+            got_index = getWordIdx(words[_id].split()[e1], word2Idx)
+            wordvec.append(got_index)
+        else:
+            got_index = getWordIdx(words[_id].split()[e1-1], word2Idx)
+            wordvec.append(got_index)
+        
+        got_index = getWordIdx(words[_id].split()[e1+1], word2Idx)
+        wordvec.append(got_index)
+        got_index = getWordIdx(words[_id].split()[e2-1], word2Idx)
+        wordvec.append(got_index)
+        if e2 == len(words[_id].split()):
+            got_index = getWordIdx(words[_id].split()[e2], word2Idx)
+            wordvec.append(got_index)
+        else:
+            got_index = getWordIdx(words[_id].split()[e2-1], word2Idx)
+            wordvec.append(got_index)
+
+        pos1_idx = get_pos_index(pos_pos1_list, words[_id].split()[e1])
+        pos2_idx = get_pos_index(pos_pos2_list, words[_id].split()[e2])
+        pos_position1Matrix.append(pos1_idx)
+        pos_position2Matrix.append(pos2_idx)
         for word in range(min(maxsen_len, len(words[_id].split()))):
             tokens[word] = getWordIdx(words[_id].split()[word], word2Idx)
             dis1 = word - int(e1)
@@ -159,10 +256,14 @@ def create_data(word2Idx, flag):
         tokenMatrix.append(tokens)
         position1Matrix.append(pos1Value)
         position2Matrix.append(pos2Value)
+        position1_5Word.append(wordvec)
+    #print(np.array(position1_5Word).shape)
+    #print(np.array(pos_position1Matrix).shape)
+    #print(np.array(pos_position2Matrix).shape)
     if flag == 'train':
-        return np.array(tokenMatrix, dtype='int32'), np.array(position1Matrix, dtype='int32'), np.array(position2Matrix, dtype='int32'), label
+        return np.array(tokenMatrix, dtype='int32'), np.array(position1Matrix, dtype='int32'), np.array(position2Matrix, dtype='int32'), np.array(position1_5Word, dtype='int32'), label
     elif flag == 'test':
-        return np.array(tokenMatrix, dtype='int32'), np.array(position1Matrix, dtype='int32'), np.array(position2Matrix, dtype='int32')
+        return np.array(tokenMatrix, dtype='int32'), np.array(position1Matrix, dtype='int32'), np.array(position2Matrix, dtype='int32'), np.array(position1_5Word, dtype='int32')
     
 
 def getWordIdx(word, word2Idx):
@@ -172,8 +273,23 @@ def getWordIdx(word, word2Idx):
 
 
 if __name__ == '__main__':
-    test_word, test_pos1, test_pos2 = load_test_data(test_path)
+    #test_word, test_pos1, test_pos2 = load_test_data(test_path)
     with open('word2IDx.pickle','rb') as f:
         word2Idx = pickle.load(f)
+    '''
     train_token, train_pos1, train_pos2, train_y= create_data(word2Idx, 'train')
     test_token, test_pos1, test_pos2 = create_data(word2Idx, 'test')
+    '''
+    train_word, pos1, pos2, label = load_train_data(train_path)
+    create_pos_set(train_word, pos1, pos2)
+    #pos_pos1_list, pos_pos2_list = create_pos_set(train_word, pos1, pos2)
+    '''
+    with open('pos1_list.pkl','rb') as f:
+        pos_pos1_list = pickle.load(f)
+    with open('pos2_list.pkl','rb') as f:
+        pos_pos2_list = pickle.load(f)
+    '''
+    #print(len(pos_pos1_list))
+    #print(len(pos_pos2_list))
+    #train_token, train_pos1, train_pos2, train_POS1, train_POS2, train_y= create_data(word2Idx, 'train', pos_pos1_list, pos_pos2_list)
+    #test_token, test_pos1, test_pos2, test_POS1, test_POS2 = create_data(word2Idx, 'test', pos_pos1_list, pos_pos2_list)
